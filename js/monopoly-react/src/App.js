c8a1cb7c184e103e86ef6bc1d3e83286aecb6319
varun93 <1hvarun@gmail.com>
2019-03-28 17:23:52 -0400

    sample monopoly application

--- js/monopoly-react/src/App.js
+++ js/monopoly-react/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
+import * as constants from "./constants";
 import "./App.css";
 const autobahn = require("autobahn");
 
@@ -7,10 +7,14 @@ class App extends Component {
   constructor(props, context) {
     super(props, context);
     this.session = null;
+    this.state = {
+      actionButton: ""
+    };
   }
+
   componentWillMount() {
-    const url = "ws://127.0.0.1:80/ws";
-    const realm = "realm1";
+    const url = constants.ROUTER_ENDPOINT;
+    const realm = constants.APPLICATION_REALM;
 
     const connection = new autobahn.Connection({
       url,
@@ -25,43 +29,99 @@ class App extends Component {
     connection.open();
   }
 
-  trade = state => {
-    console.log("In Trade");
+  /* Receivers  */
+  receiveTradeRequest = state => {
+    this.setState({ actionButton: "trade" });
     console.log(state);
   };
 
-  auction = state => {
-    console.log("In Auction");
+  receiveAuctionRequest = state => {
+    this.setState({ actionButton: "auction" });
     console.log(state);
   };
 
-  bsm = state => {
-    console.log("In BSM");
+  receiveBSMRequest = state => {
+    this.setState({ actionButton: "bsm" });
     console.log(state);
   };
 
-  jailDecision = state => {
-    console.log("In Jail Decision");
+  receiveJailDecisionRequest = state => {
+    this.setState({ actionButton: "jail-decision" });
     console.log(state);
+  };
+
+  /* Send Response; action listners  */
+
+  sendTradeResponse = event => {
+    console.log(event.target);
+  };
+
+  sendAuctionResponse = event => {
+    console.log(event.target);
+  };
+
+  sendBSMResponse = event => {
+    console.log(event.target);
+  };
+
+  sendJailDecisionResponse = event => {
+    console.log(event.target);
   };
 
   subscribeToEvents = () => {
-    this.session.subscribe("monopoly.trade", this.trade);
-    this.session.subscribe("monopoly.auction", this.auction);
-    this.session.subscribe("monopoly.auction", this.bsm);
-    this.session.subscribe("monopoly.auction", this.jailDecision);
+    this.session.subscribe("monopoly.trade", this.receiveTradeRequest);
+    this.session.subscribe("monopoly.auction", this.receiveAuctionRequest);
+    this.session.subscribe("monopoly.bsm", this.receiveBSMRequest);
+    this.session.subscribe(
+      "monopoly.jail_decision",
+      this.receiveJailDecisionRequest
+    );
   };
 
-  sendMessage = () => {
-    this.session.publish("com.myapp.hello", ["Hello, world!"]);
-    console.log("Message Sent");
+  startGame = () => {
+    this.session.publish("monopoly.auction", ["Start Game"]);
   };
 
   render() {
+    const { actionButton } = this.state;
+    const {
+      sendAuctionResponse,
+      sendBSMResponse,
+      sendJailDecisionResponse,
+      sendTradeResponse
+    } = this;
     return (
       <div className="App">
         <h1>Welcome to Monopoly </h1>
-        <button onClick={this.sendMessage}> Send Message </button>
+        <button onClick={this.startGame}> Start Game </button>
+        <button
+          onClick={sendTradeResponse}
+          className="trade"
+          disabled={actionButton === "trade" ? "" : "disabled"}
+        >
+          Trade
+        </button>
+        <button
+          onClick={sendAuctionResponse}
+          className="auction"
+          disabled={actionButton === "auction" ? "" : "disabled"}
+        >
+          Auction
+        </button>
+        <button
+          onClick={sendBSMResponse}
+          className="bsm"
+          disabled={actionButton === "bsm" ? "" : "disabled"}
+        >
+          BSM
+        </button>
+        <button
+          className="jail-decision"
+          disabled={actionButton === "jail-decision" ? "" : "disabled"}
+          onClick={sendJailDecisionResponse}
+        >
+          Jail Decision
+        </button>
       </div>
     );
   }
---
+++ js/monopoly-react/src/constants.js
@@ -0,0 +1,2 @@
+export const ROUTER_ENDPOINT = "ws://127.0.0.1:80/ws";
+export const APPLICATION_REALM = "realm1";
